df54a5d0db2ee246b644e4b78799d60302fcc095
KIGAP <164750787+KIGAP@users.noreply.github.com>
2024-05-25 15:57:26 +0200

    Added new homework
    Homework 9

---
+++ HW 9 js/Script/script.js
@@ -0,0 +1,37 @@
+//let number1 = +prompt('Enter the number')
+//if(number1>0) {
+   // console.log(number1**3);
+//}
+//if(number1<0) {
+    //console.log(number1**2);
+//}
+
+
+//let number2 = +prompt('Enter the numner of pupil');
+//let number3 = +prompt('Enter the numner of chiars');
+//if(number2<=number3) {
+//console.log('стульев хватает');
+//}
+//if(number2>number3) {
+    //console.log('стульев не хватает'); 
+//}
+
+
+
+let users = ['John', 'Bob', 'Tom', 'Nick', 'Sam', 'Alex', 'Jack'];
+console.log(users[users.length -2]);
+console.log(users);
+
+users.shift(0);
+console.log(users);
+
+users.push('Landon');
+console.log(users);
+
+users[3] = 'Den';
+console.log(users);
+
+users.pop();
+console.log(users);
+
+
